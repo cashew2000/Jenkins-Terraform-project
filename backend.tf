638334e9b7bb70db4de389b270207b51009081b4
cashew2000 <cashewm24@gmail.com>
2026-02-14 16:48:41 +0530

    Change S3 key for Terraform state file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "assignment1-terraform-state-bucket"
-    key            = "assessment-4/dev/terraform.tfstate"
+    key            = "jenkins/dev/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
