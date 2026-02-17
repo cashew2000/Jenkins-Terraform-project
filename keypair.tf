673245b7fdeecf27234dd2ce60f3a2eac45051c2
cashew2000 <cashewm24@gmail.com>
2026-02-17 11:33:30 +0530

    Uncomment AWS key pair resource configuration

--- keypair.tf
+++ keypair.tf
@@ -1,11 +1,11 @@
-#resource "aws_key_pair" "devops_key" {
-#  key_name   = "devops-assignment3-${var.stage}"
+resource "aws_key_pair" "devops_key" {
+  key_name   = "devops-assignment3-${var.stage}"
 
   # 👇 Replace the path if your .pub file is elsewhere
-  #public_key = file("${path.module}/devops-assignment3.pub")
+  public_key = file("${path.module}/devops-assignment3.pub")
 
-  #tags = {
-    #Name  = "devops-assignment3-key"
-    #Stage = var.stage
-  #}
-#}
+  tags = {
+    Name  = "devops-assignment3-key"
+    Stage = var.stage
+  }
+}
